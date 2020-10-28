4a2dd4b708ae227c8e932beff4c4f614c6afe103
Dongpeng Ma <dongpeng.ma@shopee.com>
2020-10-28 18:26:07 +0800

    feat: add use effect not trigger

---
+++ src/UseEffectNotTrigger/index.tsx
@@ -0,0 +1,35 @@
+import React, { FC, useEffect, useState } from 'react'
+import classNames from 'classnames'
+
+import styles from './style.module.css'
+
+export interface IUseEffectNotTriggerProps {
+}
+
+function getSearchResult (t: string) {
+  return Promise.reject('')
+}
+
+const UseEffectNotTrigger: FC<IUseEffectNotTriggerProps> = ({}: IUseEffectNotTriggerProps) => {
+  const [searchText, setSearchText] = useState('')
+  const [stableText, setStableText] = useState(searchText)
+  const [result, setResult] = useState('')
+  useEffect(() => {
+    async function fn() {
+      // 如果请求失败了, 再次点击按钮, 不会再发请求, 因为stableText没有变化
+      // 发请求要放在click函数里面!
+      const result = await getSearchResult(stableText)
+      setResult(result)
+    }
+    fn()
+  }, [stableText])
+  return (
+    <div>
+      <input type="text" value={searchText} onChange={e => setSearchText(e.target.value)}/>
+      <button onClick={() => setStableText(searchText)}>search</button>
+      <div>result: {result}</div>
+    </div>
+  )
+}
+
+export default UseEffectNotTrigger
